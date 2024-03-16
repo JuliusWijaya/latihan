d452c99afabb0d3601a8395b96614f3a2d8914a0
Julius Wijaya <juliuswijaya51@gmail.com>
2024-03-16 21:57:57 +0700

    first commit

---
+++ TestingApp/Controllers/JurusanController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestingApp.Models;
+
+namespace TestingApp.Controllers
+{
+    public class JurusanController : Controller
+    {
+        private DbStudentEntities _context = new DbStudentEntities();
+        // GET: Jurusan
+        public ActionResult Index(string message)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                //ViewBag.Message = message;
+                TempData["message"] = message;
+            }
+
+            var jurusans = _context.Jurusans.ToList();
+
+            return View(jurusans);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Jurusan model)
+        {
+            _context.Jurusans.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Jurusan", new { message = $"Data {model.Id_Jurusan} berhasil disimpan" });
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            ViewBag.Jrs = _context.Jurusans.Where(x => x.Id == id).FirstOrDefault();
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Jrs = _context.Jurusans.Where(x => x.Id == id).FirstOrDefault();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Jurusan model)
+        {
+           var data = _context.Jurusans.Where(x => x.Id == model.Id).FirstOrDefault();
+
+            if(data != null)
+            {
+                data.Id_Jurusan = model.Id_Jurusan;
+                data.Nama_Jurusan = model.Nama_Jurusan;
+                _context.SaveChanges();
+            } else
+            {
+                TempData["message-error"] = $"Data {model.Id_Jurusan} gagal diupdate!";
+                return RedirectToAction("Index");
+            }
+
+            TempData["message"] = $"Data {model.Id_Jurusan} berhasil diupdate";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var data = _context.Jurusans.Where(x => x.Id == id).FirstOrDefault();
+            _context.Jurusans.Remove(data);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Jurusan", new { message = $"Data {data.Nama_Jurusan} berhasil dihapus" });
+        }
+    }
+}
---
+++ TestingApp/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestingApp.Models;
+
+namespace TestingApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public DbStudentEntities db = new DbStudentEntities(); 
+        // GET: Student
+
+        [HttpGet]
+        public ActionResult Index(string message)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                //ViewBag.Message = message;
+                TempData["message"] = message;
+            }
+
+            var data = db.Students.ToList();
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            DbStudentEntities myEntity = new DbStudentEntities();
+            var getJurusans = myEntity.Jurusans.ToList();
+            SelectList list = new SelectList(getJurusans, "Id", "Nama_Jurusan");
+            ViewData["Jurusans"] = list;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student model)
+        {
+            db.Students.Add(model);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Student", new { message = $"Mahasiswa {model.Name} baru berhasil ditambahkan!"});
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            ViewBag.Student = db.Students.Where(x => x.Id == id).FirstOrDefault();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id) 
+        {
+            var data = db.Students.Where(x => x.Id == id).FirstOrDefault();
+            db.Students.Remove(data);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Student", new { message = $"Mahasiswa {data.Name} berhasil didelete!" });
+        }
+    }
+}
